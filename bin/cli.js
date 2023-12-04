d020f17ba320991b3660bcbd63707676a0e70113
Tristan Vu <tristan.vu@zendesk.com>
2023-12-04 17:29:35 -0500

    fix

--- bin/cli.js
+++ bin/cli.js
@@ -14,7 +14,7 @@ if (process.argv.length < 3) {
 const projectName = process.argv[2];
 const currentPath = process.cwd();
 const projectPath = path.join(currentPath, projectName);
-const git_repo = "https://github.com/tristan-vu/test";
+const git_repo = "https://github.com/tristan-vu/create-zaf-app";
 
 try {
     fs.mkdirSync(projectPath);
