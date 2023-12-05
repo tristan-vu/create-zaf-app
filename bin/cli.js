cfe0be1ac824aadee70359e1fd03fbf3b4a6f409
Tristan Vu <tristan.vu@zendesk.com>
2023-12-04 19:26:59 -0500

    fix

--- bin/cli.js
+++ bin/cli.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 if (process.argv.length < 3) {
     console.log('You have to provide a name to your app.');
     console.log('For example :');
-    console.log('    npx create-my-boilerplate my-app');
+    console.log('npx create-my-boilerplate my-app');
     process.exit(1);
 }
 
